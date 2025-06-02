8b7a42a5667d3ad983dbeea19d160b5edd3bce71
camilo pereira <camilodev1990@gmail.com>
2025-06-01 20:56:19 -0500

    se realiza el trabajo de la issue. Closes #1

--- probandoapps/urls.py
+++ probandoapps/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.HomeView.as_view(), name="home"),
     path("usuario/", include("apps_importadas.appusuario.urls")),
 ]
